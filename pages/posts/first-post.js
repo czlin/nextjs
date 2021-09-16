c7bdd28634ca629bf072fac222e8a748cadf37c1
linchuzhonglcz@163.com <linchuzhonglcz@163.com>
2021-09-16 17:03:38 +0800

    初始框架

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>xxx</h1>
+      <a>Back to home</a>
+    </Layout>
+  ) 
+}
